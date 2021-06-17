70ecafe2189234199299a30ff27ef483dea023ce
RafaelCV <cavr134@gmail.com>
2021-06-17 10:59:19 -0500

    firebase auth register and login

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,31 +1,89 @@
-import { types } from '../types/types';
-import { firebase, googleAuthProvider } from '../firebase/firebase-config';
+import Swal from 'sweetalert2';
 
-export const startLoginEmailPassword= (email, password) => {
+import { firebase, googleAuthProvider } from '../firebase/firebase-config';
+import { types } from '../types/types';
+import { startLoading, finishLoading } from './ui';
+
+
+export const startLoginEmailPassword = (email, password) => {
+    return (dispatch) => {
+
+        dispatch( startLoading() );
+        
+        
+        firebase.auth().signInWithEmailAndPassword( email, password )
+            .then( ({ user }) => {
+                dispatch(login( user.uid, user.displayName ));
+
+                dispatch( finishLoading() );
+            })
+            .catch( e => {
+                console.log(e);
+                dispatch( finishLoading() );
+                Swal.fire('Error', e.message, 'error');
+            })
+
+        
+        
+    }
+}
+
+export const startRegisterWithEmailPasswordName = ( email, password, name ) => {
     return ( dispatch ) => {
-        setTimeout(() => {
-            dispatch(login(123, 'Pedro'));
-        }, 3500);
-    };
-};
+
+        firebase.auth().createUserWithEmailAndPassword( email, password )
+            .then( async({ user }) => {
+
+                await user.updateProfile({ displayName: name });
+
+                dispatch(
+                    login( user.uid, user.displayName )
+                );
+            })
+            .catch( e => {
+                console.log(e);
+                Swal.fire('Error', e.message, 'error');
+            })
+
+    }
+}
+
+
 
 export const startGoogleLogin = () => {
     return ( dispatch ) => {
+
         firebase.auth().signInWithPopup( googleAuthProvider )
-            .then(({user} )=> {
+            .then( ({ user }) => {
                 dispatch(
-                    login(user.uid, user.displayName)
-                );
+                    login( user.uid, user.displayName )
+                )
             });
+
     }
 }
 
-export const login = (uid, displayName) => {
-    return {
-        type: types.login,
-        payload: {
-            uid,
-            displayName 
-        }
+
+export const login = (uid, displayName) => ({
+    type: types.login,
+    payload: {
+        uid,
+        displayName
+    }
+});
+
+
+export const startLogout = () => {
+    return async( dispatch ) => {
+        await firebase.auth().signOut();
+
+        dispatch( logout() );
     }
 }
+
+
+export const logout = () => ({
+    type: types.logout
+})
+
+
